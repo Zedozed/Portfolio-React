4da3d8749e6ed720e6d69c91f6d7f81ae79d6dfa
zedozed <zelalemc6@gmail.com>
2023-11-24 09:58:03 +0300

    first commit

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -12,7 +12,7 @@ import "react-pdf/dist/esm/Page/TextLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc =
   "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.11.338/pdf.worker.js";
 
-const resumeLink = `https://raw.githubusercontent.com/Zedozed/portfolio/main/src/assets/zedo.pdf`;
+const resumeLink = `https://raw.githubusercontent.com/Zedozed/React-Portfolio/main/src/assets/zedo.pdf`;
 
 const Resume = () => {
   const [width, setWidth] = useState(1200);
